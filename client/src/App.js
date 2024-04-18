cb727c0e1d7b58200d77a62a1fc2b744c0788c35
mgardanier <mgardanier@gmail.com>
2024-04-17 18:11:59 -0600

    Fix time calculation

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,19 @@ function App() {
     )
   }, [])
 
+  function getExpirationCountdown(expirationDate) {
+    const today = new Date()
+    const miliseconds = (expirationDate - today)
+    const timeLeft = (miliseconds * 1000) > 3600 ? new Date(miliseconds).toISOString().substring(11, 16) : new Date(miliseconds).toISOString().substring(14, 19)
+    return formatExpirationCountdown(timeLeft)
+  }
+
+  function formatExpirationCountdown(timeLeftString) {
+    debugger
+    const units = timeLeftString.split(':')
+    return units.length === 3 ? `${units[0]}D ${units[1]}H ${units[2]}M` : `${units[0]}H ${units[1]}M`
+  }
+
   return (
     <div>
       {(typeof backendData.title === 'undefined') ? (
@@ -22,7 +35,7 @@ function App() {
           <h1>{backendData.title}</h1>
           <p>{backendData.briefing}</p>
           {/* This time calc isn't working right... */}
-          <p>Operation expires in {new Date((new Date(backendData.expiration) - new Date()) * 1000).toISOString().slice(11, 16)}</p>
+          <p>Operation expires in {getExpirationCountdown(new Date(backendData.expiration))}</p>
           <br></br>
         </div>
       )}
